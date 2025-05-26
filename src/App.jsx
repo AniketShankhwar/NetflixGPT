795bb7b61cbce183b0bacd4dd89ec8d6158881b2
AniketShankhwar <aniketshankhwar1531@gmail.com>
2025-05-26 16:59:03 +0530

    Initial commit: setup Vite + React + Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl ">Hi This is React App</h1>
+    </>
+   )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: [
+    './App.jsx',                  // if you’re using the root html
+    './src/**/*.{js,ts,jsx,tsx}',    // your React components
+  ],
+  theme: { /* … */ },
+  plugins: [ /* … */ ],
+}
